9bda216322920c2f8d51a2175a6be336b795b384
Danylo Borysenko <danborys@c1r5s4.42prague.com>
2026-02-03 13:32:52 +0100

    ex2 basic class structure was created

---
+++ ex2/nexus_pipeline.py
@@ -0,0 +1,73 @@
+from typing import List, Dict, Union, Any, Protocol
+from abc import ABC, abstractmethod
+
+
+class ProcessingStage(Protocol):
+    def process(self, data: Any) -> Any:
+        ...
+
+
+class InputStage:
+    def process(self, data: Any) -> Dict:
+        pass
+
+
+class TransformStage():
+    def process(self, data: Any) -> Dict:
+        pass
+
+
+class OutputStage:
+    def process(self, data: Any) -> str:
+        pass
+
+
+class ProcessingPipeline(ABC):
+    def __init__(self):
+        super().__init__()
+        self.stages: List[ProcessingStage] = []
+
+    def add_stage(self, stage: ProcessingStage) -> None:
+        self.stages.append(stage)
+
+    @abstractmethod
+    def process(self, data: Any) -> Any:
+        pass
+
+
+class JSONAdapter(ProcessingPipeline):
+    def __init__(self, pipeline_id: str):
+        super().__init__()
+        self.pipeline_id = pipeline_id
+
+    def process(self, data: Any) -> Union[str, Any]:
+        pass
+
+
+class CSVAdapter(ProcessingPipeline):
+    def __init__(self, pipeline_id: str):
+        super().__init__()
+        self.pipeline_id = pipeline_id
+
+    def process(self, data: Any) -> Union[str, Any]:
+        pass
+
+
+class StreamAdapter(ProcessingPipeline):
+    def __init__(self, pipeline_id: str):
+        super().__init__()
+        self.pipeline_id = pipeline_id
+
+    def process(self, data: Any) -> Union[str, Any]:
+        pass
+
+
+class NexusManager:
+    def __init__(self):
+        self.pipelines: List[ProcessingPipeline] = []
+
+    def add_pipeline(self, pipeline: ProcessingPipeline) -> None:
+        self.pipelines.append(pipeline)
+
+    def process_data(self, data: Any) -> None:
+        pass
